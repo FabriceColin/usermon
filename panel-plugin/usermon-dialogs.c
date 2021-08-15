a86d1ac22dc4b09a17d28b1928cb3a32ee692e92
Fabrice Colin <fabrice.colin@gmail.com>
2021-08-15 08:58:24 +0200

    Tweaked preferences and text strings in line with other Xfce plugins.

--- panel-plugin/usermon-dialogs.c
+++ panel-plugin/usermon-dialogs.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2003-2020 Fabrice Colin
+ *  Copyright 2003-2021 Fabrice Colin
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -88,26 +88,29 @@ static GtkWidget *xfce_usermon_create_layout(UserMonitorPlugin * usermon_plugin)
 	GtkWidget *row2 =
 	    gtk_box_new(GTK_ORIENTATION_HORIZONTAL, DEFAULT_USERMON_PADDING);
 	GtkWidget *max_users_count_label =
-	    gtk_label_new(_("Critical Number Of Users"));
+	    gtk_label_new(_("Critical number of users"));
 	GtkWidget *max_users_count_spin =
 	    gtk_spin_button_new_with_range(1, 100, 1);
-	GtkWidget *alarm_period_label = gtk_label_new(_("Alarm Period"));
+	GtkWidget *alarm_period_label = gtk_label_new(_("Alarm period"));
 	GtkWidget *alarm_period_spin =
 	    gtk_spin_button_new_with_range(5, 3600, 5);
-	GtkWidget *alarm_period_label_post = gtk_label_new(_("Seconds"));
+	GtkWidget *alarm_period_label_post = gtk_label_new(_("seconds"));
 
-	gtk_box_pack_start(GTK_BOX(row1), max_users_count_label, TRUE, FALSE,
-			   0);
+	gtk_box_pack_start(GTK_BOX(row1), max_users_count_label,
+			TRUE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(row1), GTK_WIDGET(max_users_count_spin),
-			   TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(vbox), row1, FALSE, FALSE, 0);
+			TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), row1,
+			FALSE, FALSE, 0);
 
-	gtk_box_pack_start(GTK_BOX(row2), alarm_period_label, TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(row2), GTK_WIDGET(alarm_period_spin), TRUE,
-			   FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(row2), alarm_period_label_post, TRUE, FALSE,
-			   0);
-	gtk_box_pack_start(GTK_BOX(vbox), row2, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(row2), alarm_period_label,
+			TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(row2), GTK_WIDGET(alarm_period_spin),
+			TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(row2), alarm_period_label_post,
+			TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), row2,
+			FALSE, FALSE, 0);
 
 	gtk_spin_button_set_value(GTK_SPIN_BUTTON(max_users_count_spin),
 				  (gdouble) usermon_plugin->max_users_count);
@@ -145,21 +148,30 @@ void xfce_usermon_configure_plugin(XfcePanelPlugin * plugin)
 	xfce_panel_plugin_block_menu(plugin);
 
 	/* create the dialog */
-	dialog = xfce_titled_dialog_new_with_buttons(_("User Monitor Plugin"),
-						     GTK_WINDOW
-						     (gtk_widget_get_toplevel
-						      (GTK_WIDGET(plugin))),
-						     GTK_DIALOG_DESTROY_WITH_PARENT,
-						     "gtk-help",
-						     GTK_RESPONSE_HELP,
-						     "gtk-close",
-						     GTK_RESPONSE_OK, NULL);
+#if LIBXFCE4UI_CHECK_VERSION(4,14,0)
+	dialog = xfce_titled_dialog_new_with_mixed_buttons(_("User Monitor"),
+				GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))),
+				GTK_DIALOG_DESTROY_WITH_PARENT,
+				"help-browser", _("_Help"), GTK_RESPONSE_HELP,
+				"window-close", _("_Close"), GTK_RESPONSE_OK,
+				NULL);
+#else
+	dialog = xfce_titled_dialog_new_with_buttons(_("User Monitor"),
+				GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(plugin))),
+				GTK_DIALOG_DESTROY_WITH_PARENT,
+				"gtk-help", GTK_RESPONSE_HELP,
+				"gtk-close", GTK_RESPONSE_OK,
+				NULL);
+#endif
+
+	/* set a subtitle */
+	xfce_titled_dialog_set_subtitle(XFCE_TITLED_DIALOG(dialog), _("Preferences"));
 
 	/* populate the dialog */
 	vbox = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
 	content = xfce_usermon_create_layout(usermon_plugin);
-	gtk_box_pack_start(GTK_BOX(vbox), content, TRUE, FALSE,
-			   DEFAULT_USERMON_PADDING);
+	gtk_box_pack_start(GTK_BOX(vbox), content,
+			TRUE, FALSE, DEFAULT_USERMON_PADDING);
 
 	/* center dialog on the screen */
 	gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER);
@@ -173,8 +185,8 @@ void xfce_usermon_configure_plugin(XfcePanelPlugin * plugin)
 
 	/* connect the reponse signal to the dialog */
 	g_signal_connect(G_OBJECT(dialog), "response",
-			 G_CALLBACK(xfce_usermon_configure_response),
-			 usermon_plugin);
+			G_CALLBACK(xfce_usermon_configure_response),
+			usermon_plugin);
 
 	/* show the entire dialog */
 	gtk_widget_show(content);
@@ -200,7 +212,7 @@ void xfce_usermon_show_about(XfcePanelPlugin * plugin)
 			      PACKAGE_NAME, "comments",
 			      _("User monitor plugin"), "website",
 			      PLUGIN_WEBSITE, "copyright",
-			      _("Copyright \xc2\xa9 2003-2020 Fabrice Colin"),
+			      _("Copyright \xc2\xa9 2003-2021 Fabrice Colin"),
 			      "authors", auth, NULL);
 
 	if (icon) {
